b6249f64d6b3465bcb5cf51f4e85d3ecbb105db9
Mike V <mike@iglab.dev>
2023-07-12 15:49:16 -0400

    13.7 error handling middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,35 +13,26 @@ router.get('/', async (_req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    return res.json(blog)
-  } catch (error) {
-    return res.status(400).json({error})
-  }
+  const blog = await Blog.create(req.body)
+  return res.json(blog)
 })
 
 router.delete('/:id', blogFinder, async (req, res) => {
   if(req.blog) {
-      try {
         await req.blog.destroy()
         res.status(204).end()
-      } catch (error) {
-        res.status(400).json({error})
-      }
   } else {
     res.status(404).end()
   }
 })
 
 router.put('/:id',blogFinder, async (req, res) => {
-  if (req.blog) {
+  if (req.body.likes) {
     req.blog.likes = req.body.likes
     await req.blog.save()
     res.json(req.blog)
-
   } else {
-    res.status(404).end
+    res.status(404).end()
   }
 })
 
