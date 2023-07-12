d1d8268e86f2d5b712fb23734df03a26d850771b
Mike V <mike@iglab.dev>
2023-07-11 22:33:30 -0400

    commit for removal of wsl

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ Blog.init({
     },
     likes: {
       type: DataTypes.INTEGER,
-    }
+    },
 }, {
   sequelize,
   underscored: true,
@@ -33,14 +33,43 @@ Blog.init({
   modelName: 'blog'
 });
 
+Blog.sync()
+
+app.use(express.json)
+
 /* Endpoints */
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Blog API is running');
 });
 
-app.get('/blogs', async (req, res) => {
-  const blogs = await Blog.findAll();
+app.get('/api/blogs', async (_req, res) => {
+    const blogs = await Blog.findAll();
+    res.json(blogs)
 });
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    console.log(req.body)
+    const note = await Blog.create(req.body)
+    return res.json(note)
+  } catch (error) {
+    return res.status(400).json({error})
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findOne(req.params.id)
+  if(blog) {
+    try {
+      await blog.destroy()
+      res.send.status(204).end()
+    } catch (error) {
+      res.status(400).json({error})
+    }
+  } else {
+    res.status(404).end()
+  }
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
