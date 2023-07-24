908fc9b1cbbda5b6785714b7ff98b9d6495d0625
Mike V <mike@iglab.dev>
2023-07-24 01:27:27 -0400

    13.22 update reading list

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -16,4 +16,25 @@ router.post('/', tokenExtractor, async (req, res) => {
   }
 })
 
+router.put('/:id', tokenExtractor, async (req, res) => {
+  try {
+    const loggedInUser = await User.findByPk(req.decodedToken.id)
+    if(!loggedInUser){
+      throw new Error('must be logged in to manipulate reading lists.')
+    }
+    const readingList = await Readinglist.findByPk(req.params.id)
+    if(!readingList){
+      throw new Error('reading list not found.')
+    }
+    console.log(readingList)
+    if(readingList.userId !== loggedInUser.id){
+      throw new Error('reading list does not belong to logged in user.')
+    }
+    const updatedReadingList = await readingList.update({...req.body})
+    return res.json(updatedReadingList)
+  } catch(error) {
+    return res.status(400).json({error: error.message})
+  }
+})
+
 module.exports = router
